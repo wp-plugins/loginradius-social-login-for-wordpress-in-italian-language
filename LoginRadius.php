dc8688cc57c1fef1ee31ad71700c4931c38e4d88
loginradius <loginradius@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-24 05:46:57 +0000

    Update version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/loginradius-social-login-for-wordpress-in-italian-language/tags/2.0@480137 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ LoginRadius.php
@@ -0,0 +1,356 @@
+<?php
+/*Plugin Name:LoginRadius (Social Login) for wordpress  
+Plugin URI: http://www.LoginRadius.com
+Description: LoginRadius plugin enables social login on a wordpress website letting users log in through their existing IDs such as Facebook, Twitter, Google, Yahoo and over 15 more! This eliminates long registration process i.e. filling up a long registration form, verifying email ID, remembering another username and password so your users are just one click away from logging in to your website. Other than social login, LoginRadius plugin also include User Profile Data and Social Analytics.
+Version: 1.0.3
+Author: LoginRadius Team
+Author URI: http://www.LoginRadius.com
+License: GPL2+
+*/
+include('function.php');
+include('header.php');
+include('LoginRadius_admin.php');
+include('LoginRadiusSDK.php');
+@ini_set('display_errors',0);
+$LoginRadiuspluginpath = WP_CONTENT_URL.'/plugins/'.plugin_basename(dirname(__FILE__)).'/';
+class Login_Radius_Connect {
+public static function init() {
+			add_action( 'parse_request', array(get_class(), 'connect') );
+		    add_action( 'wp_enqueue_scripts', array(get_class(), 'LoginRadius_front_css_custom_page' ) );
+			add_filter( 'LR_logout_url' , array(get_class(), 'log_out_url'), 20, 2);
+					}
+public static function LoginRadius_front_css_custom_page() {
+    wp_register_style('LoginRadius-plugin-frontpage-css', plugins_url('style.css', __FILE__), array(), '1.0.0', 'all');
+    wp_enqueue_style('LoginRadius-plugin-frontpage-css');
+ }			
+public static function log_out_url() {
+				$redirect= get_permalink();
+			    $link = '<a href="' . wp_logout_url($redirect) . '" title="'.e__('Logout').'">'.e__('Logout').'</a>';
+			    echo apply_filters('Login_Radius_log_out_url',$link);
+		                 }
+public static function connect() {
+                            $LoginRadius_secret=get_option('LoginRadius_secret');
+							$dummyemail=get_option('dummyemail');
+							$obj = new LoginRadius();
+                            $userprofile = $obj->construct($LoginRadius_secret);
+if($obj->IsAuthenticated == true && !is_user_logged_in() && !is_admin()) 
+      {
+	   $id=$userprofile->ID;
+		if(!empty($userprofile->Email[0]->Value) || $dummyemail==true)
+			{
+					 $Email=$userprofile->Email[0]->Value;
+					 $FullName=$userprofile->FullName;
+					 $ProfileName=$userprofile->ProfileName;
+					 $Fname=$userprofile->FirstName; 
+					 $Lname=$userprofile->LastName;
+					 $id=$userprofile->ID;
+					 $Provider=$userprofile->Provider;
+					 $user_pass=wp_generate_password();
+					 self::add_user($Email,$FullName,$ProfileName,$Fname,$Lname,$id,$Provider,$user_pass);
+			}
+if(empty($userprofile->Email[0]->Value) && $dummyemail==false )
+{ global $wpdb;
+$FullName=$userprofile->FullName;
+$ProfileName=$userprofile->ProfileName;
+$Fname=$userprofile->FirstName; 
+$Lname=$userprofile->LastName;
+$id=$userprofile->ID;
+$Provider=$userprofile->Provider;
+$msg="<p>Inserisci il tuo indirizzo email per procedere.</p>";
+      // look for users with the id match
+$wp_user_id = $wpdb->get_var($wpdb->prepare("SELECT user_id FROM $wpdb->usermeta WHERE meta_key='id' AND meta_value = %s",$id));
+	if ( !empty($wp_user_id) ){
+	            // set cookies manually since 
+					self::set_cookies($wp_user_id);
+				    $redirect=LoginRadius_redirect();
+				   wp_redirect($redirect);
+				   }
+else{ self::popup($FullName,$ProfileName,$Fname,$Lname,$id,$Provider,$msg); } 
+   } //check email ends
+}//autantication ends
+if(isset($_POST['LoginRadiusRedSliderClick']))
+{
+   if(email_exists($_REQUEST['email']))
+    {
+		 $msg="<p style='color:red;'>L'e-mail che hai inserito è già esistente! Inserisci un'altra e-mail.</p>";
+		 $id=$_POST['Id'];  
+		 $Fname=$_POST['fname'];
+		 $Lname=$_POST['lname'];
+		 $ProfileName=$_POST['profileName'];
+		 $FullName=$_POST['fullName'];
+		 $Provider=$_POST['provider'];
+		self::popup($FullName,$ProfileName,$Fname,$Lname,$id,$Provider,$msg);
+   }
+else{
+	$id=$_POST['Id'];  
+    $Email=urldecode($_POST['email']);
+	$Fname=$_POST['fname'];
+	$Lname=$_POST['lname'];
+	$ProfileName=$_POST['profileName'];
+	$FullName=$_POST['fullName'];
+	$Provider=$_POST['provider'];
+	$user_pass=wp_generate_password();
+    self::add_user($Email,$FullName,$ProfileName,$Fname,$Lname,$id,$Provider,$user_pass);
+    }
+}
+}//connect ends
+private static function add_user($Email,$FullName,$ProfileName,$Fname,$Lname,$id,$Provider,$user_pass)
+{
+//if anything not found correctly
+$dummyemail=get_option('dummyemail'); 
+$Email_id=substr($id,7);
+$Email_id2=str_replace("/","_",$Email_id);
+switch( $Provider ){
+		case 'facebook':
+					 $username=$Fname.$Lname;
+					 $fname=$Fname;
+					 $lname=$Lname;
+					 $email=$Email;
+                     break;
+        case 'twitter':
+				$username=$ProfileName;
+				$fname=$ProfileName;
+				$lname=$ProfileName;
+					if ($dummyemail==false){
+					$email=$Email;}
+					else{
+					$email=$id.'@'.$Provider.'.com';}
+					break;
+        case 'google':
+					$username=$Fname.$Lname;
+					$fname=$Fname;
+					$lname=$Lname;
+					$email=$Email;
+					break;
+        case 'yahoo':
+					$username=$Fname.$Lname;
+					$fname=$Fname;
+					$lname=$Lname;
+					$email=$Email;
+					break;
+        case 'linkedin':
+					$username=$Fname.$Lname;
+					$fname=$Fname;
+					$lname=$Lname;
+					if ($dummyemail==false){
+					$email=$Email;}
+					else{
+					$email=$id.'@'.$Provider.'.com';}
+					break;
+		case 'aol':
+					$user_name=explode('@',$Email);
+					$username=$user_name[0];
+					$Name=explode('@',$username);
+					$fname=str_replace("_"," ",$Name[0]);
+					$lname=str_replace("_"," ",$Name[0]);
+					$email=$Email;
+		            break;
+		case 'hyves':
+					$username=$Fname.$Lname;
+					$fname=$Fname;
+					$lname=$Lname;
+					$email=$Email;
+					break;
+		default:
+				if($Fname=='' && $Lname=='' && $FullName!='')
+				{ $Fname=$FullName;}
+				if($Fname=='' && $Lname=='' && $FullName=='' && $ProfileName!='')
+				   {$Fname=$ProfileName;}
+				$Email_id=substr($id,7);
+				$Email_id2=str_replace("/","_",$Email_id);
+				if($Fname=='' && $Lname=='' && $Email=='' && $id!='')
+				{
+				$username=$id;
+				$fname=$id;
+				$lname=$id;
+				$email=str_replace(".","_",$Email_id2).'@'.$Provider.'.com';
+				}
+					else if($Fname!='' && $Lname!='' && $Email=='' && $id!=''){
+					$username=$Fname.$Lname;
+					$fname=$Fname;
+					$lname=$Lname;
+					$email=str_replace(" ","_",$username).'@'.$Provider.'.com';
+					}
+					else if($Fname=='' && $Lname=='' && $Email!=''){
+							$user_name=explode('@',$Email);
+							$username=$user_name[0];
+							$Name=explode('@',$username);
+							$fname=str_replace("_"," ",$Name[0]);
+							$lname=str_replace("_"," ",$Name[0]);
+							$email=$Email;
+							}
+							else if($Lname=='' && $Fname!='' && $Email!=''){
+							$username=$Fname;
+							$fname=$Fname;
+							$lname=$Fname;
+							$email=$Email;
+							}
+								else {
+								$username=$Fname.$Lname;
+								$fname=$Fname;
+								$lname=$Lname;
+								$email=$Email;
+								}
+               break;
+              }
+global $wpdb;
+ $dummyemail=get_option('dummyemail');
+ //look for user with username match	
+ 						  $nameexists = true;
+						  $index = 0;
+						  $userName = $username;
+						  $first_name=$fname;
+						  while ($nameexists == true) {
+							if (username_exists($userName) != 0) {
+							  $index++;
+							  $userName = $username.$index;
+							  $first_name=$fname.$index;
+							} else {
+							  $nameexists = false;
+							}
+						  }
+						  $username=$userName;
+						 $userdata = array( 
+                               'user_login' => $username,
+							   'user_nicename' => $fname,
+							   'user_email' => $email, 
+							   'display_name' => $fname,
+                               'nickname' => $fname,
+                               'first_name' => $fname,
+							   'user_url' => home_url(),
+							   'role' => 'Subscriber'
+							   );
+// look for users with the id match
+$wp_user_id = $wpdb->get_var($wpdb->prepare("SELECT user_id FROM $wpdb->usermeta WHERE meta_key='id' AND meta_value = %s",$id));
+				if (empty($wp_user_id)) {
+					// Look for a user with the same email
+	             $wp_user_obj = get_user_by('email', $email);
+                   // get the userid from the  email if the query failed
+					$wp_user_id = $wp_user_obj->ID;
+					}
+				    if ( !empty($wp_user_id) ) {
+					// set cookies manually since wp_signon requires the username/password combo.
+					  self::set_cookies($wp_user_id);
+					  $redirect=LoginRadius_redirect();
+					  wp_redirect($redirect);
+					}
+					else {  
+					if (!empty($email)) {
+					  $user_id = wp_create_user( $username,$user_pass, $email );
+					  wp_new_user_notification($username,$user_pass);
+					  }
+                      if (! is_wp_error($user_id) ) 
+					  {
+					  if (!empty($email)) {
+					   $user = wp_signon(
+										array(
+												'user_login' =>$username,
+												'user_password' =>$user_pass,
+												'remember' => true
+											), false );
+                        do_action( 'LR_registration',$user,$username,$email,$user_pass,$userdata);
+					}
+if( is_wp_error( $user )){}else{}
+if (!empty($email)) {
+update_user_meta($user_id,'email',$email);
+}
+if (!empty($id)) {
+update_user_meta($user_id,'id',$id );
+}
+						  wp_clear_auth_cookie();
+						  wp_set_auth_cookie($user_id);
+			              wp_set_current_user($user_id);
+						  $redirect=LoginRadius_redirect();
+					      wp_redirect($redirect);
+						  } 
+else {
+	wp_redirect($redirect);
+}}
+}
+private static function popup($FullName,$ProfileName,$Fname,$Lname,$id,$Provider,$msg)
+{?>
+<div id="fade" class="LoginRadius_overlay" class="LoginRadius_content_IE">
+<div class="LoginRadius_DivPopHeader">
+<div style="float:right; border:1px solid #00CCFF;padding:2px;"><a href="" title='close' onclick="javascript:document.LoginRadius_popupDiv.style.display = 'none'">X</a></div>
+<div class="LoginRadius_popupDiv"><?php if($msg){echo "<b>".$msg."</b><br />";}?>
+					<form id="wp_login_form"  method="post"  action="">
+					<input type="text" name="email" id="email" />
+					<input type="submit" id="LoginRadiusRedSliderClick" name="LoginRadiusRedSliderClick" value="Submit">
+					<input type="hidden" name="provider" id="provider" value="<?php echo $Provider;?>" />
+					<input type="hidden" name="fname" id="fname" value="<?php echo $Fname;?>" />
+					<input type="hidden" name="lname" id="lname" value="<?php echo $Lname;?>" />
+					<input type="hidden" name="profileName" id="profileName" value="<?php echo $ProfileName;?>" />
+					<input type="hidden" name="fullName" id="fullName" value="<?php echo $FullName;?>" />
+					<input type="hidden" name="Id" id="Id" value="<?php echo $id;?>" />
+					</form></div></div></div>
+<?php }
+private static function set_cookies( $user_id = 0, $remember = true ) 
+			{   
+			   if ( !function_exists( 'wp_set_auth_cookie' ) )
+				return false;
+			   if (!$user_id)
+				return false;
+			   if ( !$user = get_userdata( $user_id ) )
+				return false;
+				wp_clear_auth_cookie();
+				wp_set_auth_cookie( $user_id, $remember );
+				wp_set_current_user( $user_id );
+				return true;
+			}
+}//class end
+add_action( 'init', array( 'Login_Radius_Connect', 'init' ));
+/**
+ * Set the Admin settings on activation on the plugin.
+ */
+if (! function_exists('esc_attr')) {
+function esc_attr( $text ) {
+return attribute_escape( $text );
+}
+}
+/**
+ * Set the default settings on activation on the plugin.
+ */
+function LoginRadius_activation_hook() {
+	global $wpdb;
+	$wpdb->query("DELETE FROM $wpdb->postmeta WHERE meta_key = 'LoginRadiusoff'");
+	return LoginRadius_restore_config(false);
+     }
+register_activation_hook(__FILE__, 'LoginRadius_activation_hook');
+
+
+/**
+ * Add the LoginRadius menu to the Settings menu
+ */
+function LoginRadius_admin_menu() {
+$page=add_options_page('LoginRadius','<b style="color:#0ccdfe;">Login</b><b style="color:#000;">Radius</b>', 8,'LoginRadius', 'LoginRadius_submenu');
+add_action ('admin_print_styles-'.$page, 'LoginRadius_admin_css_custom_page');
+}
+add_action('admin_menu', 'LoginRadius_admin_menu');
+/**
+ * Add Settings CSS
+ **/
+function LoginRadius_admin_css_custom_page() {
+    wp_register_style('LoginRadius-plugin-page-css', plugins_url('style.css', __FILE__), array(), '1.0.0', 'all');
+    wp_enqueue_style('LoginRadius-plugin-page-css');
+ }
+/**
+ * Update message, used in the admin panel to show messages to users.
+ */
+function LoginRadius_message($message) {
+	echo "<div id=\"message\" class=\"updated fade\"><p>$message</p></div>\n";
+}
+/**
+ * Add a settings link to the Plugins page, so people can go straight from the plugin page to the
+ * settings page.
+*/
+function LoginRadius_filter_plugin_actions( $links, $file ){
+	static $this_plugin;
+	if ( ! $this_plugin ) $this_plugin = plugin_basename(__FILE__);
+	
+	if ( $file == $this_plugin ){
+		$settings_link = '<a href="options-general.php?page=LoginRadius">' . __('Settings') . '</a>';
+		array_unshift( $links, $settings_link ); // before other links
+	}
+	return $links;
+}
+add_filter( 'plugin_action_links', 'LoginRadius_filter_plugin_actions', 10, 2 );?>
---
+++ readme.txt
@@ -0,0 +1,181 @@
+=== Social Login for wordpress in italian language ===
+Contributors: loginradius team, Language Translator-Test: Michelangelo Giannino.com
+Donate link: http://www.loginradius.com/
+Tags: Social login, openID, Wordpress plugin, LoginRadius, comments, share post, open authentication, facebook connect, Social Login,
+OpenID integration, Open Authentication, User Profile Data, Social Analytics, oAuth integration, Online Identity, Social Profile storage,
+single sign-on , SAAS solution, Social Sign-in, Social Login Plug-in, Facebook Connect, Google, Yahoo OpenID, Hyves openid,Linkedin openid,
+Mixi opendID, Virgilio OpenID, italian language,BuddyPress
+Requires at least: 2.5.0
+Tested up to: 3.2.1
+Stable tag: 2.0
+
+LoginRadius abilita il "Social Login" su siti Wordpress e non.
+
+== Description ==
+
+Il plugin LoginRadius consente agli utenti di accedere a un sito Wordpress, semplicemente con un click, 
+usando il proprio account Facebook, Twitter, Google, Yahoo e tantissimi altri (20 in totale). Elimina quindi un lungo
+processo di registrazione ovvero:
+
+1. Riempire un modulo (si evita soprattutto l'insopportabile captcha).
+2. Verificare l' e-mail.
+3. Ricordarsi di un ulteriore login e password.
+
+Guarda il <a href="http://www.youtube.com/watch?v=mLYbVNsU36U"  target="_blank">video loginradius</a>
+[youtube http://www.youtube.com/watch?v=mLYbVNsU36U]
+
+= Caratteristiche =
+
+* Login Sociale
+* Dati profilo utente quali: Nickname, email (solo per alcuni social), IP.
+* Social Analytics (Coming Soon!)
+* GRATUITO
+* Non richiede conoscenze di programmazione.
+* Personalizzazioni per utenti "Premium" (a pagamento).
+* Disponibile non solo per i cms quali Wordpress e Joomla! ma integrabile anche su cms ad hoc.
+* supporto 24ore/24 x 7gg/7
+
+
+= Elenco dei fornitori di ID =
+* Facebook
+* Google
+* Twitter
+* AOL
+* Flickr
+* Yahoo
+* Linkedin
+* Wordpress
+* Blogger
+* MyOpenID
+* LiveJournal
+* VeriSign
+* OpenID
+* Hyves
+* Mixi
+* MySpace
+* Orange
+* StackExchange
+* SteamCommunity
+* Virgilio
+
+
+= Links =
+* Website: http://www.loginradius.com
+* Wordpress live demo: http://wordpressdemo.loginradius.com
+* Altri live demo: http://www.loginradius.com/demo
+* Press/Media page: http://www.loginradius.com/press
+* Blog: http://blog.loginradius.com
+* I nostri video: http://www.youtube.com/loginradius
+
+
+== Installation ==
+
+1. Scaricate il plugin e decomprimite il contenuto.
+2. Caricate il contenuto nella cartella "plugins" del vostro sito wordpress (es. http://www.nome_sito.it/wp-content/plugins/)  
+3. Attivate il plugin attraverso la pagina "plugin" nel pannello di amministrazione raggiungile all'indirizzo http://www.nome_sito.it/wp-admin/
+4. Dopo aver attivato il plugin, apparira' una nuova voce denominata "LoginRadius" nel menu "Impostazioni". Completate la configurazione inserendo negli appositi campi l'API Key e API Key Secret (vedi punto 8).
+
+
+
+= Configurazione del Widget =
+
+5. Una volta configurato il plugin, accedere alla sezione "Aspetto" -> "Widget" 
+6. Cercate il widget di 'LoginRadius' e trascinatelo nella sidebar.
+7. Dopo l'aggiunta e' necessario impostare e/o modificare alcune semplici opzioni (titolo, testo da visualizzare nell'header/footer del widget o renderlo invisibile se l'utente e' loggato).
+
+
+
+= Configurazione =
+
+8. Accedere alla sezione "Impostazioni" e cliccate sulla voce "LoginRadius" dove andremo ad inserire l'API key e l'API Secret.
+9. Cliccate sul link LoginRadius (o collegatevi al sito www.loginradius.com) per ottenere l'API key e l'API Secret. Effettuate il login usando qualsiasi account social. 
+
+- Inserite l'url assoluto del vostro sito (esempio: http://www.nome_sito.it) e l'url della pagina da visualizzare dopo che un utente ha effettuato l'accesso (esempio: http://www.nome_sito.it/benvenuto.html). 
+- Selezionate i social login da inserire nel widget, spuntando le relative caselle.
+- Infine cliccate su "Save and get code".
+
+10. Copiate e incollate le due API key negli appositi campi.
+
+
+= Impostazione Commenti =
+
+Se si desidera che solo gli utenti registrati possano commentare, spuntate la voce "Gli utenti devono essere registrati e fare il login per poter inviare commenti", in "Impostazioni" -> "Discussione" dal pannello admin di Wordpress.
+
+
+
+= Impostare LoginRadius su una pagina =
+
+Se desiderate inserire LoginRadius su qualsiasi pagina, usare la chiamata Login_Radius_Connect_button ();
+
+
+
+==  Frequently Asked Questions ==
+
+= Che cos'e' LoginRadius? =
+
+LoginRadius e' un Software As A Service (SAAS), che permette agli utenti di accedere a un sito Web (siti web, CMS e CMS ad hoc) tramite account social come Google, Facebook, Yahoo, AOL tanti altri (20 in totale).
+
+= Per quanto tempo posso usare il plugin? =
+
+Per tutto il tempo che si usa LoginRadius. E' possibile rimuovere LoginRadius dal vostro sito web e cancella il vostro account in qualsiasi momento.
+
+
+= Come posso contattare il Team di LoginRadius? =
+
+Se avete domande o dubbi riguardanti LoginRadius, scrivere a hello@loginradius.com.
+
+
+= Quanto costa? =
+
+E' gratuito e rimarra' tale, ma per funzionalita' avanzate e soluzioni personalizzate, sono disponibili vari pacchetti. Vi preghiamo di contattarci per ulteriori dettagli.
+
+
+= Avete un sito live demo? =
+
+Si', si prega di visitare il nostro sito WordPress demo live al http://wordpressdemo.loginradius.com! Prova ad accedere e a commentare :-)
+
+Grazie per aver scaricato il nostro plugin ^_^
+
+== Screenshots ==
+
+1. Screenshot-1: L' immagine mostra l'interfaccia Loginradius per la sezione di login (Frontend). Usare le freccie laterali di scorrimento per scegliere quale social usare per accedere al sito. /tags/1.0.3/screenshot-1.png
+
+2. Screenshot-2: L' immagine mostra l'interfaccia Loginradius per la sezione commenti. Usare le freccie laterali di scorrimento per scegliere quale social usare per accedere al sito. /tags/1.0.3/screenshot-2.png
+
+3. Screenshot-3: L' immagine mostra l'interfaccia Loginradius per la sezione di login (Backend). Usare le freccie laterali di scorrimento per scegliere quale social usare per accedere al sito. /tags/1.0.3/screenshot-3.png
+
+
+== Changelog ==
+
+= 2.0 =
+* Most stable release
+* Bug fixes and Admin enhancements
+
+= 1.0.3 =
+* Bug fixed - Registered user email notification issue
+
+= 1.0.2 =
+* Bug fixed - Email pop-up issue
+
+= 1.0.1 =
+* Added js directory
+
+= 1.0 =
+* This is the first version of the application.
+
+== Upgrade Notice ==
+
+= 1.0 =
+This is the first version of the application.
+
+= 1.0.1 = 
+Added js directory
+
+= 1.0.2 = 
+Bug fixed - Email pop-up issue
+
+= 1.0.3 =
+Bug fixed - Registered user email notification issue
+
+= 2.0 =
+Bug fixes and Admin interface enhancements
